71f8162c84a1c57a7939a898d384d7aabce94e22
GabrielGLevine <gabrielglevine@gmail.com>
2016-02-21 15:41:11 -0600

    finished lexicon_analysis method. Added nltk_vader method (good general sentiment analysis). Started naive bayesian classifer on movie review dataset

--- sentiment.py
+++ sentiment.py
@@ -2,11 +2,17 @@
 
 import nltk
 import csv
+import os
+import string
+import nltk.sentiment.vader as vd
 from tweets import *
+
 lexicon_filename = 'effectwordnet/EffectWordNet.csv'
+pos_corpus = 'tagged_movies/pos'
+neg_corpus = 'tagged_movies/neg'
 
 
-def lexicon_analysis(tweets,lexicon_filename):
+def lexicon_analysis(query,lexicon_filename):
     '''
     Use EffectWordNet lexicon to classify value of tweet
     Inputs:
@@ -18,11 +24,66 @@ def lexicon_analysis(tweets,lexicon_filename):
         lex_reader = csv.reader(f,delimiter='\t')
         lexicon = {}
         for row in lex_reader:
-            lexicon[row[3]] = effect_conversion[row[2]]
+            try:
+                lexicon[row[2]] = effect_conversion[row[1]]
+            except:
+                print(row[2],row[1])
+    tweets = query.tweets
 
     for tweet in tweets:
         tweet_score = 0
-        for word in tweet.text.split():
+        for word in tweet.text.lower().split():
             if word in lexicon:
                 tweet_score += lexicon[word]
-        tweet.rate(tweet_score/abs(tweet_score))
+        if tweet_score == 0:
+            tweet.rate = 0
+        else:
+            tweet.rate = tweet_score/abs(tweet_score)
+
+    avg_rating = 0
+    num_valenced = 0
+    for tweet in tweets:
+        avg_rating += tweet._rate
+        num_valenced += (tweet._rate != 0)
+    query.avg_rate = (avg_rating/float(num_valenced)) * 50 + 50
+
+def train_naive_bayes(query,pos_corpus,neg_corpus):
+    rootdir = os.path.abspath(pos_corpus)
+    pos_rev = []
+    for l, s, files in os.walk(rootdir):
+        for file in files:
+            with open(pos_corpus+'/'+file) as f:
+                pos_rev.append((f.read().translate(None, string.punctuation), 'positive'))
+
+    rootdir = os.path.abspath(neg_corpus)
+    neg_rev = []
+    for l, s, files in os.walk(rootdir):
+        for file in files:
+            with open(neg_corpus+'/'+file) as f:
+                neg_rev.append((f.read().translate(None, string.punctuation), 'negative'))
+
+    reviews = []
+    for (words, sentiment) in pos_rev + neg_rev:
+        words_filtered = [word.lower() for word in words.split() if len(word) >= 3]
+        reviews.append((words_filtered, sentiment))
+
+    all_words = []
+    for (words, sentiment) in reviews:
+        all_words.extend(words)
+    wordlist = nltk.FreqDist(all_words)
+    word_features = wordlist.keys()
+
+def nltk_vader(query):
+    sid = vd.SentimentIntensityAnalyzer()
+    for tweet in query.tweets:
+        scores = sid.polarity_scores(tweet.text)
+        tweet.rate = scores['compound']
+
+    avg_rating = 0
+    num_valenced = 0
+    for tweet in query.tweets:
+        avg_rating += tweet._rate
+        num_valenced += (tweet._rate != 0)
+    print(avg_rating)
+    print(num_valenced)
+    query.avg_rate = (avg_rating/float(num_valenced)) * 50 + 50
