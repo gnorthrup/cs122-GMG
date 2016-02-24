7e496c8ded88da11156d62d07f46146b696fd57d
GabrielGLevine <gabrielglevine@gmail.com>
2016-02-23 22:20:50 -0600

    Completed bayesian classifier trainer. Doesn't work great. Pushing for posterity

--- DjangoThings/outer_bird/get_rating/sentiment.py
+++ DjangoThings/outer_bird/get_rating/sentiment.py
@@ -1,18 +1,18 @@
-#Sentiment Analysis
+# Sentiment Analysis
 
 import nltk
 import csv
 import os
 import string
 import nltk.sentiment.vader as vd
-from tweets import *
+import nltk
 
 lexicon_filename = 'effectwordnet/EffectWordNet.csv'
-pos_corpus = 'tagged_movies/pos'
-neg_corpus = 'tagged_movies/neg'
+pos_corpus = 'tagged_reviews/pos'
+neg_corpus = 'tagged_reviews/neg'
 
 
-def lexicon_analysis(query,lexicon_filename):
+def lexicon_analysis(query, lexicon_filename):
     '''
     Use EffectWordNet lexicon to classify value of tweet
     Inputs:
@@ -21,13 +21,13 @@ def lexicon_analysis(query,lexicon_filename):
     '''
     effect_conversion = {"+Effect": 1, "-Effect": -1, "Null": 0}
     with open(lexicon_filename) as f:
-        lex_reader = csv.reader(f,delimiter='\t')
+        lex_reader = csv.reader(f, delimiter='\t')
         lexicon = {}
         for row in lex_reader:
             try:
                 lexicon[row[2]] = effect_conversion[row[1]]
             except:
-                print(row[2],row[1])
+                print(row[2], row[1])
     tweets = query.tweets
 
     for tweet in tweets:
@@ -38,40 +38,57 @@ def lexicon_analysis(query,lexicon_filename):
         if tweet_score == 0:
             tweet.rate = 0
         else:
-            tweet.rate = tweet_score/abs(tweet_score)
+            tweet.rate = tweet_score / abs(tweet_score)
 
     avg_rating = 0
     num_valenced = 0
     for tweet in tweets:
         avg_rating += tweet._rate
         num_valenced += (tweet._rate != 0)
-    query.avg_rate = (avg_rating/float(num_valenced)) * 50 + 50
+    query.avg_rate = (avg_rating / float(num_valenced)) * 50 + 50
 
-def train_naive_bayes(query,pos_corpus,neg_corpus):
+
+def train_naive_bayes(pos_corpus, neg_corpus):
     rootdir = os.path.abspath(pos_corpus)
     pos_rev = []
     for l, s, files in os.walk(rootdir):
         for file in files:
-            with open(pos_corpus+'/'+file) as f:
-                pos_rev.append((f.read().translate(None, string.punctuation), 'positive'))
+            with open(pos_corpus + '/' + file) as f:
+                pos_rev.append(
+                    (f.read().translate(None, string.punctuation), 'positive'))
 
     rootdir = os.path.abspath(neg_corpus)
     neg_rev = []
     for l, s, files in os.walk(rootdir):
         for file in files:
-            with open(neg_corpus+'/'+file) as f:
-                neg_rev.append((f.read().translate(None, string.punctuation), 'negative'))
+            with open(neg_corpus + '/' + file) as f:
+                neg_rev.append(
+                    (f.read().translate(None, string.punctuation), 'negative'))
 
     reviews = []
     for (words, sentiment) in pos_rev + neg_rev:
-        words_filtered = [word.lower() for word in words.split() if len(word) >= 3]
+        words_filtered = [word.lower()
+                          for word in words.split() if len(word) >= 3]
         reviews.append((words_filtered, sentiment))
 
     all_words = []
     for (words, sentiment) in reviews:
         all_words.extend(words)
     wordlist = nltk.FreqDist(all_words)
+    global word_features
     word_features = wordlist.keys()
+    training_set = nltk.classify.apply_features(extract_features, reviews)
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+    return classifier
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
 
 def nltk_vader(query):
     sid = vd.SentimentIntensityAnalyzer()
@@ -89,4 +106,4 @@ def nltk_vader(query):
     if num_valenced == 0:
         pass
     else:
-        query.avg_rate = (avg_rating/float(num_valenced)) * 50 + 50
+        query.avg_rate = (avg_rating / float(num_valenced)) * 50 + 50
