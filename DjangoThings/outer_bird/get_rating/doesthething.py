8cff59b0f527ea905a15f19680dc1a3414ce758d
GabrielGLevine <gabrielglevine@gmail.com>
2016-03-06 13:25:27 -0600

    rounding

--- DjangoThings/outer_bird/get_rating/doesthething.py
+++ DjangoThings/outer_bird/get_rating/doesthething.py
@@ -1,11 +1,12 @@
 import get_rating.tweets
 import get_rating.sentiment
+import numpy as np
 
 def thething(string):
 	if len(string) != 0:
 		query = get_rating.tweets.Query(string)
 		get_rating.tweets.collect_tweets(query,600)
 		get_rating.sentiment.nltk_vader(query)
-		return (str(query.avg_rate),query.best.text,query.worst.text)
+		return (str(np.round(query.avg_rate,2)),query.best.text,query.worst.text)
 	else:
 		return ('','','')
